b5e026839613468e14d58c42c89aa4c947d438f2
nikolas3000 <kolya.eremenko93@gmail.com>
2025-03-07 16:21:35 +0300

    add actuator/prometheus metrics

---
+++ src/test/java/com/example/kafkaTopicListener/KafkaTopicListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaTopicListener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTopicListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
